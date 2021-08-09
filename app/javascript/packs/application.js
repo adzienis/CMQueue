b97e1c5331beb8e687b8247916a34b735bbf84a7
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-08-08 21:32:47 -0400

    updated credentials

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import Rails from "rails-ujs";
 import "@hotwired/turbo";
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "controllers";
