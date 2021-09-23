646b22b7fa850b7324c683e52cbd64c2a9a9070a
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-09-23 17:50:41 -0400

    added migrations

---
+++ db/migrate/20210917210147_create_tag_groups.rb
@@ -0,0 +1,11 @@
+class CreateTagGroups < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tag_groups do |t|
+      t.references :course, null: false, foreign_key: true
+      t.string :name, null: false
+      t.jsonb :validations
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210917210227_add_tag_group_to_tags.rb
@@ -0,0 +1,10 @@
+class AddTagGroupToTags < ActiveRecord::Migration[6.1]
+  def up
+
+    add_reference :tags, :tag_group, index: true, foreign_key: true
+  end
+
+  def down
+    remove_reference :tags, :tag_group
+  end
+end
