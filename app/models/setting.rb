13615eb2f7a7d0df5e963290762f7d03a447d64d
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-09-24 19:46:52 -0400

    updated websocket callback for settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -68,11 +68,11 @@ class Setting < ApplicationRecord
     case resource_type
     when "Course"
       QueueChannel.broadcast_to resource_type.constantize.find(resource_id), {
-        invalidate: ['settings']
+        invalidate: ['courses', resource_id, 'settings']
       }
     when "User"
       SiteChannel.broadcast_to resource_type.constantize.find(resource_id), {
-        invalidate: ['settings']
+        invalidate: ['users', resource_id, 'settings']
       }
     end
   end
