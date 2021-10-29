97d64a9a845597b7a671ad483931d0ba09b2e98c
Arthur Dzieniszewski <arthurdzieniszewski@gmail.com>
2021-10-28 23:56:10 -0400

    updated search bar component

--- app/components/shared/search/search_bar_component.rb
+++ app/components/shared/search/search_bar_component.rb
@@ -1,11 +1,20 @@
 class Shared::Search::SearchBarComponent < ViewComponent::Base
-  def initialize(resources:, course:, query_parameters:)
+  def initialize(resources:, course:, query_parameters:, options:{})
     @resources = resources
     @course = course
     @query_parameters = query_parameters
+    @options = options
+  end
+
+  def actions
+    @actions ||= options[:actions] || {}
+  end
+
+  def resource
+    @resource ||= resources.to_s.singularize.to_sym
   end
 
   private
 
-  attr_reader :resources, :course, :query_parameters
+  attr_reader :resources, :course, :query_parameters, :options
 end
